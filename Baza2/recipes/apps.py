48e8cb397ccfe014a96096e30a334bf4b54f9bcb
Maciej Steckiewicz <steckiewicz.maciek@gmail.com>
2024-06-19 19:47:36 +0200

    Update apps.py

--- Baza2/recipes/apps.py
+++ Baza2/recipes/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class RecipesConfig(AppConfig):
+    """
+    Konfiguracja aplikacji Django dla modułu 'recipes'.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recipes'
