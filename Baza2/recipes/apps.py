33fd53b31c2960347b112edcf41a5084a2c58e46
Maciej Steckiewicz <steckiewicz.maciek@gmail.com>
2024-07-06 23:27:51 +0200

    Delete Baza2 directory

--- Baza2/recipes/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class RecipesConfig(AppConfig):
-    """
-    Konfiguracja aplikacji Django dla modułu 'recipes'.
-    """
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'recipes'
